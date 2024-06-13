d8234ad855430c0472b8a727cf94f554bf9314a4
Ourang-Zeb Khan <khanourangzaib@gmail.com>
2024-06-13 12:31:22 +0300

    API Constants

---
+++ NetworkingKit/Sources/NetworkingKit/ApiConstants.swift
@@ -0,0 +1,15 @@
+//
+//  Request.swift
+//  MVVMSample
+//
+//  Created by Khan on 03/03/2023.
+//
+
+import Foundation
+
+public struct ApiConstants {
+    static let apiKey = "181af7fcab50e40fabe2d10cc8b90e37"
+    static let baseUrl = URL(string: "https://api.themoviedb.org/3")!
+    static let originalImageUrl = URL(string: "https://image.tmdb.org/t/p/original")!
+    static let smallImageUrl = URL(string: "https://image.tmdb.org/t/p/w154")!
+}
