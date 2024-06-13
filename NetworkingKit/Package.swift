6f0ed08e7b6d96a3315deec6d04bae161fa30d54
Ourang-Zeb Khan <khanourangzaib@gmail.com>
2024-06-13 12:29:48 +0300

    xcode project

---
+++ NetworkingKit/Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "NetworkingKit",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "NetworkingKit",
+            targets: ["NetworkingKit"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "NetworkingKit"),
+        .testTarget(
+            name: "NetworkingKitTests",
+            dependencies: ["NetworkingKit"]),
+    ]
+)
