ab33cee9f373fb74570d60b739d7aa1459d08d0e
ourangzeb <khanourangzaib@gmail.com>
2024-08-15 09:42:08 +0300

    utility

--- NetworkingKit/Tests/NetworkingKitTests/NetworkUtilitiesTests.swift
+++ NetworkingKit/Tests/NetworkingKitTests/NetworkUtilitiesTests.swift
@@ -14,8 +14,22 @@ final class NetworkUtilitiesTests: XCTestCase {
     
     
     
+    func testResourceRequest() {
     
-    
+        
+        let url = URL(string: "https://api.example.com/resource")!
+               let parameters: [String: CustomStringConvertible] = ["query": "test", "page": 1]
+               let resource = Resource<Decodable>(url: url, parameters: parameters, httpMethod: .get)
+               
+               guard let request = resource.makeRequest() else {
+                   XCTFail("Failed to create URLRequest")
+                   return
+               }
+               
+               XCTAssertEqual(request.url?.absoluteString, "https://api.example.com/resource?query=test&page=1")
+               XCTAssertEqual(request.httpMethod, "GET")
+               XCTAssertNil(request.httpBody)
+    }
     
     
 
