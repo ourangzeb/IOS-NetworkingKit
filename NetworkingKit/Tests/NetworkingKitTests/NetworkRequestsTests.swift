3ffe6f43653fc0b295ce79cf101924873e5ad1f3
ourangzeb <khanourangzaib@gmail.com>
2024-08-19 12:42:08 +0300

    test cases with parameters for network requests

--- NetworkingKit/Tests/NetworkingKitTests/NetworkRequestsTests.swift
+++ NetworkingKit/Tests/NetworkingKitTests/NetworkRequestsTests.swift
@@ -17,17 +17,34 @@ final class NetworkRequestsTests: XCTestCase {
     
                 
     func testGETRequestWithoutParameters() {
-        
-        
         guard let url = URL(string: ApiConstants.baseUrl) else { return
             XCTFail()
         }
          let networkRequest = NetworkRequest<TestModel>.init(url: url)
-            
         XCTAssertEqual(networkRequest.url.absoluteString, ApiConstants.baseUrl)
         XCTAssertEqual(networkRequest.httpMethod.rawValue, "GET")
         XCTAssertNil(networkRequest.request?.httpBody, "GET request should not have abody")
     }
+    
+    func testGETRequestWithParameters() {
+        guard let url = URL(string: ApiConstants.baseUrl) else { return
+            XCTFail()
+        }
+        let parameters: [String: CustomStringConvertible] = ["key": "value"]
+        let networkRequest = NetworkRequest<TestModel>.init(url: url, parameters: parameters, httpMethod: .post)
+        XCTAssertEqual(networkRequest.url.absoluteString, ApiConstants.baseUrl)
+        XCTAssertEqual(networkRequest.httpMethod.rawValue, "POST")
+        // Convert parameters to JSON data
+        let jsonData: Data
+        do {
+            jsonData = try JSONSerialization.data(withJSONObject: parameters, options: [])
+        } catch {
+            XCTFail("Failed to serialize parameters to JSON data: \(error)")
+            return
+        }
+        XCTAssertEqual(networkRequest.request?.httpBody,jsonData )
+    }
+    
     
     
     
