b52837b7d8f9d09f7953372b2a3c9fdba3b7aa60
ourangzeb <khanourangzaib@gmail.com>
2024-08-19 10:19:09 +0300

    Mutable API constants

--- NetworkingKit/Sources/NetworkingKit/ApiConstants.swift
+++ NetworkingKit/Sources/NetworkingKit/ApiConstants.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 public struct ApiConstants {
-    static let apiKey = "181af7fcab50e40fabe2d10cc8b90e37"
-    static let baseUrl = URL(string: "https://api.themoviedb.org/3")!
-    static let originalImageUrl = URL(string: "https://image.tmdb.org/t/p/original")!
-    static let smallImageUrl = URL(string: "https://image.tmdb.org/t/p/w154")!
+    public static var apiKey = "181af7fcab50e40fabe2d10cc8b90e37"
+       public static var baseUrl = URL(string: "https://api.themoviedb.org/3")!
+       public static var originalImageUrl = URL(string: "https://image.tmdb.org/t/p/original")!
+       public static var smallImageUrl = URL(string: "https://image.tmdb.org/t/p/w154")!
 }
